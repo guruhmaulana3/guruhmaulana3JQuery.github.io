06f6f46f8bafa9989d804dfe943ede08c92ae153
guruhmaulana <112224529+guruhmaulana@users.noreply.github.com>
2022-09-08 06:06:21 +0700

    upload pertama

---
+++ script.js
@@ -0,0 +1,38 @@
+function popup(){
+    alert("Anda Yakin Ingin Menghapusnya??")
+    confirm("Anda telah berhasil menghapusnya")
+}
+
+function hello(){
+    alert("Ingin Mengubah NIM Anda??")
+    prompt("Silahkan Masukkan NIM Anda Dengan Benar")
+    confirm("Anda berhasil mengubah NIM")
+}
+
+function ainun(){
+    alert("Anda mengklik Ainun Putri Nurhaliva")
+}
+
+function aji(){
+    alert("Anda mengklik Aji Sudrajat")
+}
+
+function devi(){
+    alert("Anda mengklik Devi Mustika Asti")
+}
+
+function dewi(){
+    alert("Anda mengklik Dewi Basmallah")
+}
+
+function dimas(){
+    alert("Anda mengklik Dimas Ardiansyah")
+}
+
+function kelas(){
+    alert("Anda mengklik KA-2021-P1")
+}
+
+function alamat(){
+    alert("Anda mengklik Cirebon")
+}
